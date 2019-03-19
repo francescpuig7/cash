4f1b7005590ced8398923f0471ab35c6000601d6
Francesc Puig <francescpuig7@gmail.com>
2019-03-19 23:32:46 +0100

    Add iva type

--- product.py
+++ product.py
@@ -1,11 +1,13 @@
 PRODUCT_TYPES = ['BAR', 'RESTAURANT']
 
 class Product:
-    def __init__(self, name, price, category=None):
+    def __init__(self, name, price, category=None, iva=None):
         self.name = str(name)
         self.price = float(price)
         if category:
             self.category = category
+        if iva:
+            self.iva = iva
 
     def __repr__(self):
         return '{0} {1}'.format(self.name, self.price)
