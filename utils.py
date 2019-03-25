c1053a391f98994afd6953da043ef83e3e403995
Francesc Puig <francescpuig7@gmail.com>
2019-03-25 23:37:24 +0100

    Add license

---
+++ utils.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import pandas as pd
+from os import path
+
+class Utils(object):
+
+    def __init__(self):
+        pass
+
+    def check_license(self, code, dt):
+        print(code)
+        if datetime.now().strftime('%d-%m-%y') <= dt:
+            return True
+        else:
+            return False
+
+    def check_code(self, code):
+        filename = path.join(path.dirname(path.realpath(__file__)), 'keys.csv')
+        df_keys = pd.read_csv(filename)
+        codes = list(df_keys['code'])
+        if code in codes:
+            return True
+        else:
+            return False
